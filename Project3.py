48f99fdefde50f617bf77c3c6d245f49e868700d
Steven Yeh <Stevenstuff@stevens-mbp.attlocal.net>
2015-12-10 23:16:09 -0500

    12/10

--- Project3.py
+++ Project3.py
@@ -13,11 +13,58 @@ from collections import defaultdict
 
 osmfile = '/Users/Stevenstuff/Downloads/raleigh_north-carolina.osm'
 
-lower = re.compile(r'^([a-z]|_)*$')
-lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
-problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
-CREATED = ["version", "changeset", "timestamp", "user", "uid"]
+def fix_street(street_name):
+    street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+
+    expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road",
+                "Trail", "Parkway", "Commons", "Plaza", "Center", "Bypass", "Circle", "Way", "54", "55",
+                "Suite", "Hill", "Hills", "Extension", "West", "East", "North", "South", "Fork"]
+
+    mapping = {"St": "Street",
+               "St.": "Street",
+               "st": "Street",
+               "Ave.": "Avenue",
+               "Ave": "Avenue",
+               "avenue": "Avenue",
+               "AVE": "Avenue",
+               "ave": "Avenue",
+               "Blvd.": "Boulevard",
+               "Blvd": "Boulevard",
+               "Rd": "Road",
+               "Rd.": "Road",
+               "CIrcle": "Circle",
+               "Dr.": "Drive",
+               "Dr": "Drive",
+               "Pl": "Place",
+               "Pl.": "Place",
+               "Ext": "Extension",
+               "Ext.": "Extension"}
+    m = street_type_re.search(street_name)
+    if m and m.group() not in expected and m.group in mapping.keys():
+        return street_name.replace(m.group(), mapping[m.group()])
+    else:
+        return street_name
+
+
+def fix_postcode(postcode):
+    # search postcode for five digits, hyphen, four digits
+    postcode_type_re = re.compile(r'^\d{5}-\d{4}$')
+    n = postcode_type_re.search(postcode)
+    if n:
+        return postcode.split("-")[0]
+    else:
+        return postcode
+
+
+def fix_housenumber(housenumber):
+    # search housenumbers for one to many digits, hyphen, one to many letters (ignore case)
+    housenumber_re = re.compile(r'^\d+-[a-zA-Z]+$')
+    h = housenumber_re.search(housenumber)
+    if h:
+        housenumber = housenumber.replace("-", "")
+        housenumber = housenumber.upper()
+    return housenumber
 
 
 def shape_element(element):
@@ -44,10 +91,12 @@ def shape_element(element):
             value = tag.attrib["v"]
             if "addr" in tagname:
                 add_keys = tagname.split(":")
-                if add_keys[1] == "housenumber" or add_keys[1] == "postcode":
-                    address[add_keys[1]] = value
+                if add_keys[1] == "housenumber":
+                    address[add_keys[1]] = fix_housenumber(value)
+                elif add_keys[1] == "postcode":
+                    address[add_keys[1]] = fix_postcode(value)
                 elif add_keys[1] == "street" and len(add_keys) == 2:
-                    address[add_keys[1]] = value
+                    address[add_keys[1]] = fix_street(value)
             elif tagname == "amenity":
                 node["amenity"] = value
             elif tagname == "cuisine":
@@ -87,65 +136,6 @@ def process_map(file_in, pretty=False):
     return data
 
 
-street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
-
-street_types = defaultdict(int)
-
-expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road",
-            "Trail", "Parkway", "Commons", "Plaza", "Center", "Bypass", "Circle", "Way", "Highway 54"]
-
-# UPDATE THIS VARIABLE
-mapping = {"St": "Street",
-           "St.": "Street",
-           "st": "Street",
-           "Ave.": "Avenue",
-           "Ave": "Avenue",
-           "avenue": "Avenue",
-           "AVE": "Avenue",
-           "ave": "Avenue",
-           "Blvd.": "Boulevard",
-           "Rd.": "Road",
-           "CIrcle": "Circle"
-           }
-
-
-def audit_street_type(street_types, street_name):
-    m = street_type_re.search(street_name)
-    if m:
-        street_type = m.group()
-        if street_type not in expected:
-            street_types[street_type].add(street_name)
-
-
-def is_street_name(elem):
-    return elem.attrib['k'] == "addr:street"
-
-
-def audit(osmfile):
-    osm_file = open(osmfile, "r")
-    street_types = defaultdict(set)
-    for event, elem in ET.iterparse(osm_file, events=("start",)):
-
-        if elem.tag == "node" or elem.tag == "way":
-            for tag in elem.iter("tag"):
-                if is_street_name(tag):
-                    audit_street_type(street_types, tag.attrib['v'])
-
-    return street_types
-
-
-def update_name(streetname, mapping):
-    updatednames_sec = []
-    name = streetname.split()
-    for index, word in enumerate(name):
-        if word in mapping.keys():
-            updatednames_sec.append(mapping[word])
-        else:
-            updatednames_sec.append(word)
-    updatednames = ' '.join(updatednames_sec)
-    return updatednames
-
-
 def get_db(db_name):
     from pymongo import MongoClient
     client = MongoClient('localhost:27017')
@@ -154,9 +144,15 @@ def get_db(db_name):
 
 
 def make_pipeline():
-    pipeline = [{"$match": {"address.postcode": {"$exists": 1}}},
-                {"$group": {"_id": "$address.postcode", "count": {"$sum": 1}}},
-                {"$sort": {"count": -1}}]
+    # Group and sort postcodes by descending count
+    # pipeline = [{"$match": {"address.postcode": {"$exists": 1}}},
+    # {"$group": {"_id": "$address.postcode", "count": {"$sum": 1}}},
+    # {"$sort": {"count": -1}}]
+
+    # Sift out house numbers with hyphens and letters in the housenumbers field
+    # pipeline = [{"$project": {"address.housenumber": {"$substr": ["$address.housenumber", 0, -1]}}},
+    # {"$sort": {"address.housenumber": 1}},
+    # {"$match": {"address.housenumber": {"$regex": '[-a-zA-Z]'}}}]
 
     return pipeline
 
@@ -166,8 +162,9 @@ def aggregate(db, pipeline):
 
 
 if __name__ == '__main__':
+    process_map(osmfile, True)
     db = get_db('raleigh')
-    pipeline = make_pipeline()
-    result = aggregate(db, pipeline)
+    # pipeline = make_pipeline()
+    # result = aggregate(db, pipeline)
 
-    pprint.pprint(result)
+    # pprint.pprint(result)
